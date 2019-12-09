b4f7af5d0aa620243bb1b1de5bed5fa9f72a130a
Thanadej Phadtong <authanadej@gmail.com>
2019-12-09 16:59:57 +0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState, useCallback } from "react";
+
+import logo from "./logo.svg";
+import "./App.css";
+
+const LIFF_ID = "1467527063-PVMv135N";
 
 const App: React.FC = () => {
+  const [loading, setLoading] = useState(false);
+  const [profile, setProfile] = useState();
+
+  const inifLiff = useCallback(async () => {
+    setLoading(true);
+    await liff.init({
+      liffId: LIFF_ID
+    });
+    if (liff.isLoggedIn()) {
+      setProfile(await liff.getProfile());
+    } else {
+      if (process.env.NODE_ENV === "production") {
+        liff.login();
+      } else {
+        liff.login({
+          redirectUri: "http://localhost:3000"
+        });
+      }
+    }
+    setLoading(false);
+  }, []);
+
+  useEffect(() => {
+    inifLiff();
+  }, []);
+
+  console.log("profile", profile);
+
   return (
     <div className="App">
+      {loading && <p>Loading...</p>}
+
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>{profile && profile.displayName}</h1>
+        <img src={profile && profile.pictureUrl} />
       </header>
     </div>
   );
-}
+};
 
 export default App;
